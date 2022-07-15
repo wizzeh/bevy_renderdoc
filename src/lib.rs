354ee936e2756415071c8349b5bd18dfb0e7e668
Viktor Pramberg <viktor.pramberg@gmail.com>
2022-07-15 21:17:14 +0200

    Only launch one instance of replay ui
    Also added way to start an `App` with RD with an arbitrary function, in case that is ever needed. `with_renderdoc()` still uses `App::new()` though.
    
    Also started writing some documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,73 @@
+//! Wrapper around [`RenderDoc`] for Bevy.
+//!
+//! Provides an easy way to register RenderDoc with a Bevy application.
+//! Allows the user to launch the RenderDoc UI on capture, which makes
+//! taking captures more convenient.
+#![warn(missing_docs)]
 use bevy::prelude::*;
 pub use renderdoc::*;
+use sysinfo::{Pid, ProcessRefreshKind, SystemExt};
 
-pub struct RenderDocPlugin;
-
-pub struct RenderDocReplayUi(pub u32);
-
-// We need to inject RenderDoc before any windows or devices have been created.
-// From what I could tell, there's no way of doing that, so the next best thing
-// for having this plugin be a simple drag and drop is by modifying the `new()` function.
+/// Trait for creating a Bevy [`App`] with RenderDoc properly initialized.
+///
+/// We need load RenderDoc before any windows or devices have been created.
+/// From what I could tell, there's no way of doing that using a [`Plugin`], so
+/// having the user create the [`App`] using this trait is the next best thing.
 pub trait AddRenderDocPlugin {
-    fn with_renderdoc<V: Version + 'static>() -> Self;
+    /// Initializes [`RenderDoc`] and registers the plugin with an [`App`].
+    /// The app is created using [`App::new()`].
+    ///
+    /// # Examples
+    /// ```
+    /// use bevy::prelude::*;
+    /// use bevy_renderdoc::*;
+    ///
+    /// App::with_renderdoc::<V110>().run();
+    /// ```
+    fn with_renderdoc<V: Version + 'static>() -> App {
+        App::with_renderdoc_custom::<V>(App::new)
+    }
+
+    /// Initializes [`RenderDoc`] and registers the plugin with an [`App`] returned from `f`.
+    ///
+    /// # Examples
+    /// ```
+    /// use bevy::prelude::*;
+    /// use bevy_renderdoc::*;    
+    ///
+    /// App::with_renderdoc_custom::<V110>(App::new).run();
+    /// ```
+    fn with_renderdoc_custom<V: Version + 'static>(f: fn() -> App) -> App;
 }
 
 impl AddRenderDocPlugin for App {
-    fn with_renderdoc<V: Version + 'static>() -> Self {
+    fn with_renderdoc_custom<V: Version + 'static>(f: fn() -> App) -> App {
         // This needs to happen before App::new()
         let rd = RenderDoc::<V>::new();
-        let mut app = App::new();
+        let mut app = f();
 
-        app.insert_non_send_resource(rd.unwrap())
-            .add_startup_system(setup_renderdoc)
-            .add_system(trigger_capture);
+        // TODO: The tracing crate doesn't work here. Figure out why, and use info!/error! macros here.
+        match rd {
+            Ok(rd) => {
+                app.insert_non_send_resource(rd);
+                println!("Initialized RenderDoc successfully!");
+            }
+            Err(e) => {
+                println!("Failed to initialize RenderDoc: {:?}", e);
+            }
+        };
 
+        app.add_plugin(RenderDocPlugin);
         app
     }
 }
 
+struct RenderDocPlugin;
 impl Plugin for RenderDocPlugin {
-    fn build(&self, _: &mut App) {}
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(setup_renderdoc)
+            .add_system(trigger_capture);
+    }
 }
 
 fn setup_renderdoc(rd: Option<NonSendMut<RenderDoc<V110>>>) {
@@ -41,17 +81,30 @@ fn setup_renderdoc(rd: Option<NonSendMut<RenderDoc<V110>>>) {
 }
 
 fn trigger_capture(
-    key: Res<Input<KeyCode>>,
+    key: Option<Res<Input<KeyCode>>>,
     rd: Option<NonSend<RenderDoc<V110>>>,
-    mut replay_pid: Local<u32>,
+    mut replay_pid: Local<usize>,
+    mut system: Local<sysinfo::System>,
 ) {
-    // avoid launching multiple instances of replay ui
-    if rd.is_none() || *replay_pid != 0 {
+    if rd.is_none() || key.is_none() {
         return;
     }
 
     let rd = rd.unwrap();
-    if key.just_pressed(KeyCode::F12) {
-        *replay_pid = rd.launch_replay_ui(true, None).unwrap();
+    if key.unwrap().just_pressed(KeyCode::F12) {
+        // Avoid launching multiple instances of the replay ui
+        if system
+            .refresh_process_specifics(Pid::from(*replay_pid), ProcessRefreshKind::new().with_cpu())
+        {
+            return;
+        }
+
+        match rd.launch_replay_ui(true, None) {
+            Ok(pid) => {
+                *replay_pid = pid as usize;
+                info!("Launching RenderDoc Replay UI");
+            }
+            Err(e) => error!("Failed to launch RenderDoc Replay UI: {:?}", e),
+        }
     }
 }
